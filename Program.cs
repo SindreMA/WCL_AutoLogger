ef856f7ca1874e6dfc396f13d6fd8c1ac3ae093b
Sindre Andreassen <sindrema@gmail.com>
2022-08-29 22:23:57 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Management;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WCL_AutoLogger
+{
+    class Program
+    {
+        internal static string WCL_Executable;
+        internal static string log_path;
+        static void Main(string[] args)
+        {
+            if (args.Count() == 2)
+            {
+                log_path = args[1];
+                WCL_Executable = args[0];
+                processWatcher();
+                Console.WriteLine("Watcher is running...click enter to stop it");
+                Console.ReadLine();
+            } else
+            {
+                Console.WriteLine("You need to pass exe path to WCL and the log location");
+            }
+        }
+        static void processWatcher()
+        {
+            ManagementEventWatcher processStartEvent = new ManagementEventWatcher("SELECT * FROM Win32_ProcessStartTrace");
+            ManagementEventWatcher processStopEvent = new ManagementEventWatcher("SELECT * FROM Win32_ProcessStopTrace");
+            processStartEvent.EventArrived += new EventArrivedEventHandler(processStartEvent_EventArrived);
+            processStopEvent.EventArrived += new EventArrivedEventHandler(processStopEvent_EventArrived);
+            processStopEvent.Start();
+            processStartEvent.Start();
+        }
+        static void processStartEvent_EventArrived(object sender, EventArrivedEventArgs e)
+        {
+            string processName = e.NewEvent.Properties["ProcessName"].Value.ToString();
+            string processID = Convert.ToInt32(e.NewEvent.Properties["ProcessID"].Value).ToString();
+            Console.WriteLine("Process started. Name: " + processName + " | ID: " + processID);
+
+            if (processName.ToLower().Replace(".exe", "") == "wow")
+            {
+                var wcl = new WCL_Handler(WCL_Executable, log_path);
+                wcl.StartLiveLog();
+            }
+        }
+
+        static void processStopEvent_EventArrived(object sender, EventArrivedEventArgs e)
+        {
+            string processName = e.NewEvent.Properties["ProcessName"].Value.ToString();
+            string processID = Convert.ToInt32(e.NewEvent.Properties["ProcessID"].Value).ToString();
+            Console.WriteLine("Process stopped. Name: " + processName + " | ID: " + processID);
+
+            if (processName.ToLower().Replace(".exe","") == "wow")
+            {
+                var wcl = new WCL_Handler(WCL_Executable, log_path);
+                wcl.StopLiveLog();
+                wcl.DeleteLog();
+            }
+        }
+
+    }
+}
---
+++ WCL_Handler.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WCL_AutoLogger
+{
+    class WCL_Handler
+    {
+        private string wCL_Executable;
+        private string log_path;
+        public WCL_Handler(string wCL_Executable, string log_path)
+        {
+            this.wCL_Executable = wCL_Executable;
+            this.log_path = log_path;
+
+        }
+
+        public void StartLiveLog()
+        {
+            if (File.Exists(wCL_Executable))
+            { 
+                DateTime x = DateTime.Now;
+                Process.Start(wCL_Executable, $@"livelog=""{x.ToString("dd/MM/yyyy HH:mm:ss")}""");
+            } else
+            {
+                Console.WriteLine("ERROR: WCL EXECUTABLE DOESN*T EXIST");
+            }
+        }
+        public void StopLiveLog()
+        {
+            var name = Path.GetFileName(wCL_Executable).Replace(".exe", "");
+            var processes = Process.GetProcessesByName(name);
+            foreach (var process in processes)
+            {
+                process.Kill();
+            }
+        }
+
+        internal void DeleteLog()
+        {
+            if (File.Exists(log_path))
+            {
+                File.Delete(log_path);
+            } else
+            {
+                Console.WriteLine("Tried to delete log, but could not find it");
+            }
+        }
+    }
+}
